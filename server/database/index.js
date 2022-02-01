e6adbb6bdb3bce51e61d1e425d56ce4b88b77a1d
Jake <jacobandrewminor@gmail.com>
2022-02-01 11:44:20 +0000

    Add bill, journey and location models

--- server/database/index.js
+++ server/database/index.js
@@ -3,9 +3,12 @@ const environment = process.env.NODE_ENV;
 const mongoose = require("mongoose");
 mongoose.Promise = global.Promise;
 const dbConfig = require("../config/db.config.js");
+const journey = require("../models/journey")(mongoose)
+const bill = require("../models/bill")(mongoose)
+const location = require("../models/location")(mongoose)
 
 // Create mongoose and read in config
-const db = {};
+const db = {journey: journey, bill: bill, location: location};
 db.mongoose = mongoose;
 db.url = dbConfig.url;
 
---
+++ server/models/journey.js
@@ -0,0 +1,29 @@
+﻿const haversine = require('haversine-distance')
+
+module.exports = mongoose => {
+ const journeySchema = mongoose.Schema(
+   {
+    regNumber: {
+     type: "String",
+     required: [true, "A car registration number is required"]
+    },
+    entryLocation: {
+     type: mongoose.Schema.Types.ObjectId, 
+     ref: "location"
+    },
+    exitLocation: {
+     type: mongoose.Schema.Types.ObjectId, 
+     ref: "location"
+    },
+    journeyDateTime: {
+     type: Date
+    }
+   }
+ )
+ 
+ journeySchema.methods.getJourneyDistance = function() {
+  return haversine(this.entryLocation.coordinates, this.exitLocation.coordinates)
+ }
+ 
+ return mongoose.model("journey", journeySchema)
+}
