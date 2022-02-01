c95d590a5587cbe248b9c7e5d9c0e67da91199ff
jarrodback <jarrodback@users.noreply.github.com>
2022-02-01 17:32:49 +0000

    Deploying to gh-pages from @ jarrodback/CSSD-Assignment@16b7f146ec3063265dbc41d4729603ec30519017 🚀

--- server/config/db.config.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-    dev: {
-        url: "mongodb://localhost:27017/highwaytrackerdb",
-    },
-    test: {
-        url: "mongodb://localhost:27017/highwaytrackerdb_testing",
-    },
-};
