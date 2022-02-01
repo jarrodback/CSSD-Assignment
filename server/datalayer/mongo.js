707d12eb26c8df91ac1f64202344add6778d24cc
Jake <jacobandrewminor@gmail.com>
2022-02-01 13:56:31 +0000

    Create database interaction methods

--- server/datalayer/mongo.js
+++ server/datalayer/mongo.js
@@ -5,6 +5,24 @@ class DataLayer {
     }
 
     /**
+     * Find all records in the database.
+     */
+    async findAllAndPopulate(filter, populateFilter) {
+        return this.model.find(JSON.parse(JSON.stringify(filter)))
+          .limit(filter.limit)
+          .skip(filter.offset * filter.limit)
+          .populate(JSON.parse(JSON.stringify(populateFilter)))
+          .catch(error => { throw new Error(error.message)});
+    }
+    
+    /**
+     * Find a record by property in the database.
+     */
+    async findByProperty(propertyToFind) {
+        return this.model.find(propertyToFind);
+    }
+    
+    /**
      * Create and save the record to the database.
      */
     async create(recordToCreate) {
@@ -12,10 +30,13 @@ class DataLayer {
     }
 
     /**
-     * Find a record by property in the database.
+     *  Update and save the record to the database.
      */
-    async findByProperty(propertyToFind) {
-        return this.model.find(propertyToFind);
+    async update(recordId, recordToUpdate) {
+        return this.model.findByIdAndUpdate(recordId, recordToUpdate)
+          .orFail("Bill can't be found in the database.")
+          .catch(error => {throw new Error(error.message)});
+
     }
 }
 
