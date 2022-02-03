9cf6f3c65408e25a63640555e51938c2bd563248
Jake <jacobandrewminor@gmail.com>
2022-02-03 10:50:30 +0000

    HT-13 Create axios request to interact with the api

---
+++ ui/src/api/api.js
@@ -0,0 +1,28 @@
+﻿import axios from 'axios'
+
+const api = class Api {
+ constructor() {
+  this.baseUrl = 'http://localhost:3000'
+ }
+ 
+ async getAllBills(queryString) {
+  const query = {
+   driver: queryString.driver,
+   paid: queryString.paid,
+   limit: queryString.limit,
+   offset: queryString.offset
+  }
+  
+  return axios.get(`${this.baseUrl}/bill/`, { params: JSON.parse(JSON.stringify(query)) } )
+    .then(response => {return response.data})
+    .catch(error => {throw error})
+ }
+
+ async payBill(billId) {
+  return axios.put(`${this.baseUrl}/bill/${billId}`, { body: { paid: true }})
+    .then(response => {return response.data})
+    .catch(error => {throw error})
+ }
+}
+
+export default new api
