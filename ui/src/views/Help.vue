c4e25d619210a945c80842a05c0d2717386fc570
Jake <jacobandrewminor@gmail.com>
2022-02-04 10:40:47 +0000

    HT-19 Add help page and routes

---
+++ ui/src/views/Help.vue
@@ -0,0 +1,13 @@
+﻿<template>
+  <div class="mt-4">
+    <h1 class="mb-4">Frequently Asked Questions (FAQ)</h1>
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from 'vue';
+
+export default Vue.extend({
+  name: "Help" //Sets the name of file.
+})
+</script>
